731185b83d7867fe105f96d6a4f078b9d06a64a7
Matheus <matheus1204h@gmail.com>
2025-03-26 10:52:08 -0300

    creating the frontend

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html',
+                  context={'name': 'Matheus'})
